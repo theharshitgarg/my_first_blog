37759c8418031a9509d7999ca405e5677abf237e
harshit garg <harshit@esthenos.com>
2016-12-18 12:21:01 +0530

    added templates for post list

--- my_first_blog/urls.py
+++ my_first_blog/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-
+from blog import views
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', views.post_list, name='post_list')
 ]
